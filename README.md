8b483ad0ff7d90821bda8c4b6216204c104eb1e1
vladislavbaimurzin <Baimurzinv.719@gmail.com>
2019-02-23 13:28:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather APP
+
+### Getting started
+Date started 23 Feb 2019 14:30 Moscow Time
+
---
+++ src/main/java/com/baimurzin/myweatherapp/MyweatherappApplication.java
@@ -0,0 +1,13 @@
+package com.baimurzin.myweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyweatherappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyweatherappApplication.class, args);
+    }
+
+}
