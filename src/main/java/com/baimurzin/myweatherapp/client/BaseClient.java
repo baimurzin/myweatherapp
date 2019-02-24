82d1e102e7a56d205e47513047ba5eda9e556ddb
vladislavbaimurzin <Baimurzinv.719@gmail.com>
2019-02-24 14:19:51 +0200

    added logging added checking before get city weather

--- src/main/java/com/baimurzin/myweatherapp/client/BaseClient.java
+++ src/main/java/com/baimurzin/myweatherapp/client/BaseClient.java
@@ -3,7 +3,6 @@ package com.baimurzin.myweatherapp.client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
