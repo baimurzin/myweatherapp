8c9771d4b3c55bf488f2c5bf211a86a74fe0faa1
vladislavbaimurzin <Baimurzinv.719@gmail.com>
2019-02-25 01:48:02 +0200

    added swagger support

---
+++ src/main/java/com/baimurzin/myweatherapp/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.baimurzin.myweatherapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+/**
+ * Swagger configuration class.
+ * Registering swagger component
+ *
+ * @author Vladislav Baimurzin
+ */
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfig {
+
+}
