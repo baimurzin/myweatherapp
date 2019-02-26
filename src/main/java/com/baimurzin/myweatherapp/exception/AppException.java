30713e4608628da1b5cc29c519738517fe7e140e
vladislavbaimurzin <baimurzin.719@gmail.com>
2019-02-27 01:19:24 +0200

    added tests 90%+

--- src/main/java/com/baimurzin/myweatherapp/exception/AppException.java
+++ src/main/java/com/baimurzin/myweatherapp/exception/AppException.java
@@ -7,23 +7,8 @@ package com.baimurzin.myweatherapp.exception;
  */
 public class AppException extends RuntimeException {
 
-    public AppException() {
-        super();
-    }
-
     public AppException(String message) {
         super(message);
     }
 
-    public AppException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public AppException(Throwable cause) {
-        super(cause);
-    }
-
-    protected AppException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
